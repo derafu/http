7d2e9ce1b350713e684fa4ccbff9d3bd993eea28
Esteban De La Fuente Rubio <esteban@delaf.cl>
2025-06-24 17:10:38 -0400

    Limpieza de docs.

--- src/Contract/ProblemDetailInterface.php
+++ src/Contract/ProblemDetailInterface.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
 * Derafu: HTTP - Standard-Compliant HTTP Library with Extended Features.
 *
-* Copyright (c) 2025 Esteban De La Fuente Rubio / Derafu <https://www.derafu.org>
+* Copyright (c) 2025 Esteban De La Fuente Rubio / Derafu <https://www.derafu.dev>
 * Licensed under the MIT License.
 * See LICENSE file for more details.
 */
